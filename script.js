a103ed25a14bc2d106af834bd54ee09f2f89af04
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-26 23:43:47 -0400

    created for loop to display the schedule rows

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var container = $('.row');
 var timeOfDay = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
 for (var i = 0; i < timeOfDay.length; i++) {
-    //create element
+    //create div .hour
     var timeOfDayEl = $('<div>');
     //add content to element
     timeOfDayEl.text(timeOfDay[i]);
@@ -11,9 +11,20 @@ for (var i = 0; i < timeOfDay.length; i++) {
     timeOfDayEl.addClass('hour');
     //append to container
     container.append(timeOfDayEl);
+
+    //create text area .time-block
+    var timeBlockEl = $('<textarea>');
+    timeBlockEl.text('');
+    timeBlockEl.addClass('time-block');
+    container.append(timeBlockEl);
+
+    //create save button .saveBtn
+    var saveBtnEl = $('<button>');
+    saveBtnEl.text('Save');
+    saveBtnEl.addClass('saveBtn');
+    container.append(saveBtnEl);
 }
 
-console.log(timeOfDayEl)
 
 //event listeners
 
