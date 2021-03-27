430ce86e2aea5089bc05a3aa3abe3ab237f2c925
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-26 23:23:51 -0400

    created for loop to display the .hour element

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 //variables
+var container = $('.row');
+var timeOfDay = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
-//functions
- function renderTimeStamp(){
-     var timeOfDay =  ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
-     //create for loop to display time stamp
- }
+for (var i = 0; i < timeOfDay.length; i++) {
+    //create element
+    var timeOfDayEl = $('<div>');
+    //add content to element
+    timeOfDayEl.text(timeOfDay[i]);
+    //add class to element
+    timeOfDayEl.addClass('hour');
+    //append to container
+    container.append(timeOfDayEl);
+}
+
+console.log(timeOfDayEl)
+
 //event listeners
-
 
 //save button stores to local storage
 //if else statement incorporating moment to get time to change color
