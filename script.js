907389dbc13d7a81c466aff2ab30d21f2a962667
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-27 15:06:15 -0400

    fixed the color change for loop for the time blocks

--- script.js
+++ script.js
@@ -1,55 +1,67 @@
 //variables
 var container = $('.row');
 var hourContainer = [
-    { hour: 9, period: "AM" },
-    { hour: 10, period: "AM" },
-    { hour: 11, period: "AM" },
-    { hour: 12, period: "PM" },
-    { hour: 1, period: "PM" },
-    { hour: 2, period: "PM" },
-    { hour: 3, period: "PM" },
-    { hour: 4, period: "PM" },
-    { hour: 5, period: "PM" },
+    { hour: 9, period: "AM", milTime: 9 },
+    { hour: 10, period: "AM", milTime: 10 },
+    { hour: 11, period: "AM", milTime: 11 },
+    { hour: 12, period: "PM", milTime: 12 },
+    { hour: 1, period: "PM", milTime: 13 },
+    { hour: 2, period: "PM", milTime: 14 },
+    { hour: 3, period: "PM", milTime: 15 },
+    { hour: 4, period: "PM", milTime: 16 },
+    { hour: 5, period: "PM", milTime: 17 },
 ];
 var today = moment();
-var time = moment().format("hh A");
+var time = moment().format("HH");
 var saveBtnEl
-
+var timeBlockEl
+console.log(time);
+console.log(hourContainer[0].milTime);
 //display today's date on the page in the header
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
-//dynamically create the schedule rows inside a for loop
-for (var i = 0; i < hourContainer.length; i++) {
-    //create div .hour and add class and content
-    var hourEl = $('<div>').addClass('hour').text(hourContainer[i].hour + " " + hourContainer[i].period);
-    //append to container
-    container.append(hourEl);
+//create a function to generate the schedule each time the page refreshes
+function generateSchedule() {
+    for (var i = 0; i < hourContainer.length; i++) {
+        //create div .hour and add class and content
+        var hourEl = $('<div>').addClass('hour').text(hourContainer[i].hour + " " + hourContainer[i].period);
+        //append to container
+        container.append(hourEl);
 
-    //create text area .time-block
-    var timeBlockEl = $('<textarea>').addClass('time-block').text('');
-    // timeBlockEl;
-    container.append(timeBlockEl);
+        //create text area .time-block
+        var timeBlockEl = $('<textarea>').addClass('time-block .description').text('');
+        // timeBlockEl;
+        container.append(timeBlockEl);
 
-    //create save button .saveBtn that is attached to the index of each row
-    var saveBtnEl = $('<button class="saveBtn" data-index="' + i + '">Save</button>');
-    container.append(saveBtnEl);
+        //create save button .saveBtn that is attached to the index of each row
+        var saveBtnEl = $('<button class="saveBtn" data-index="' + i + '">Save</button>');
+        container.append(saveBtnEl);
 
-    //write if else statement incorporating moment to get time to change color
-    if (hourContainer[i].hour + " " + hourContainer[i].period == time) {
-        timeBlockEl.addClass('present');
-    } else if (hourContainer[i].hour + " " + hourContainer[i].period < time) {
-        timeBlockEl.addClass('past');
-    } else {
-        timeBlockEl.addClass('future');
+        //write if else statement incorporating moment to get time to change color
+        if (hourContainer[i].milTime < time) {
+            timeBlockEl.addClass('past');
+        } else if (hourContainer[i].milTime == time) {
+            timeBlockEl.addClass('present');
+        } else {
+            timeBlockEl.addClass('future');
+        }
     }
 }
+//dynamically create the schedule rows inside a for loop
 
-function init() {
+generateSchedule();
 
-}
+// function init() {
+//     var scheduleItems = JSON.parse(localStorage.getItem("timeBlockEl"));
 
+// }
 
+// function currentSchedule() {
+//     console.log($(this).attr("data-index"));
+// }
+// saveBtnEl.on("click",)
 
+//When saveBtnEl is clicked, then timeBlockEl text is saved to local storage
 
 //REMAINING:
 //figure out why time is doing that
