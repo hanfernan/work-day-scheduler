5c7617b970515c63ca298642de548b32a101b4d8
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-26 21:07:57 -0400

    started writing HTML and pseudocoding

---
+++ script.js
@@ -0,0 +1,13 @@
+//variables
+
+//functions
+ function renderTimeStamp(){
+     var timeOfDay =  ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+     //create for loop to display time stamp
+ }
+//event listeners
+
+
+//save button stores to local storage
+//if else statement incorporating moment to get time to change color
+//append today's date to header with moment.js
