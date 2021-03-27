bd82edadf3d87dc43183df66c56ff16342c315df
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-27 12:30:43 -0400

    updated hourContainer array to be comprised of hour:period key value pairs and fixed the for loop so that the hour column would display correctly with this change. I also updated the textarea width in CSS.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //variables
 var container = $('.row');
-var hour = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+var hourContainer = [
+    { hour: 9, period: "AM" },
+    { hour: 10, period: "AM" },
+    { hour: 11, period: "AM" },
+    { hour: 12, period: "PM" },
+    { hour: 1, period: "PM" },
+    { hour: 2, period: "PM" },
+    { hour: 3, period: "PM" },
+    { hour: 4, period: "PM" },
+    { hour: 5, period: "PM" },
+];
 var saveBtnEl
 var today = moment();
 var time = moment().format("hh");
@@ -11,11 +21,11 @@ console.log(time);
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
 //dynamically create the schedule rows inside a for loop
-for (var i = 0; i < hour.length; i++) {
+for (var i = 0; i < hourContainer.length; i++) {
     //create div .hour
     var hourEl = $('<div>');
     //add content to element
-    hourEl.text(hour[i]);
+    hourEl.text(hourContainer[i].hour + hourContainer[i].period);
     //add class to element
     hourEl.addClass('hour');
     //append to container
@@ -36,13 +46,13 @@ for (var i = 0; i < hour.length; i++) {
 
 
 //write if else statement incorporating moment to get time to change color
-if (hour[i] === time) {
-    timeBlockEl.addClass('present');
-} else if (hour[i] < time) {
-    timeBlockEl.addClass('past');
-} else {
-    timeBlockEl.addClass('future');
-}
+// if (hourContainer.hour[i] === time) {
+//     timeBlockEl.addClass('present');
+// } else if (hourContainer.hour[i] < time) {
+//     timeBlockEl.addClass('past');
+// } else {
+//     timeBlockEl.addClass('future');
+// }
 
 //REMAINING:
 //split hour into key value pairs so you can compare the hour without AM or PM to time
